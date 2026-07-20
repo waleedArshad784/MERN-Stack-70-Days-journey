ccab8ed0810775f3b5193d13ec67e2f9f4874f01
Muhammad Waleed <muhammadwaleed.gpgc@gmail.com>
2026-07-20 16:56:04 +0500

    build a random number genrator game

---
+++ day19.js
@@ -0,0 +1,75 @@
+
+
+
+const Button1= document.getElementById("btn1");
+const Button2 = document.getElementById("btn2");
+const guess = Number(document.getElementById("guss-no").value);
+const question = document.getElementById("Qs");
+const Button3 = document.getElementById("btn3");
+
+
+const result = document.getElementById("Result");
+
+
+Button1.addEventListener('click', randomnumber);
+Button2.addEventListener('click',checkNumber);
+Button3.addEventListener('click',quite);
+
+ let random;
+
+
+  function randomnumber(){
+    const max = Number(document.getElementById("input").value);
+    random =  Math.floor( Math.random() * max) +1 ;
+   
+    return  random;
+ };
+
+
+
+
+  function quite(){
+
+    result.innerHTML = "Game Ended.";
+
+}
+
+      function checkNumber(){
+
+    const guess = Number(document.getElementById("guss-no").value);
+
+    if(guess === random){
+
+        result.style.color ="white";
+        result.style.border = "1px solid black";
+        result.style.padding = "20px";
+        result.style.borderRadius = "10px";
+        result.style.backgroundColor = "blue";
+        result.style.font = "bold";
+        result.style.fontWeight = "12px";
+
+        
+
+        result.innerHTML = `You Win! Random Number was :  ${random}`;
+
+      
+
+
+    }
+    else if(guess < random){
+
+        question.innerHTML = "Your Guess was Too Small. Try Again.";
+
+    }
+    else{
+
+        question.innerHTML = "Your Guess was Too Large. Try Again.";
+
+    }
+
+
+}
+
+
+
+
